06660133d3a8e21032fc35784c0c8e76ab129044
Jesse Ward <jesse.ward@gmail.com>
2024-02-05 23:53:46 +0000

    py3, flake, black and such.

--- discogs_banner/canvas_tools.py
+++ discogs_banner/canvas_tools.py
@@ -1,15 +1,8 @@
-import os
-import sys
-import random
-import itertools
-import argparse
-from os import listdir
-from os.path import isfile, join
-from PIL import Image
 import logging
+import os
+import random
 
-from discogs_banner.api_tools import fetch_collection, fetch_images
-
+from PIL import Image
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -18,61 +11,62 @@ THUMB_WIDTH = 100
 THUMB_LENGTH = 100
 
 ASPECT = {
-    '16x9': (1.77, 0.56),
-    '4x3': (1.33, 0.75),
-    '2x1': (2.00, 0.50),
+    "16x9": (1.77, 0.56),
+    "4x3": (1.33, 0.75),
+    "2x1": (2.00, 0.50),
 }
 
 
-def calculate_canvas(thumbs, aspect='16x9'):
+def calculate_canvas(thumbs, aspect="16x9"):
 
     try:
         horizontal = ASPECT[aspect][0]
         vertical = ASPECT[aspect][1]
     except KeyError:
-        logger.error('Invalid aspect ratio specified. aspect={ratio}'.format(
-            ratio=aspect))
+        logger.error(
+            "Invalid aspect ratio specified. aspect={ratio}".format(ratio=aspect)
+        )
         return (None, None)
 
     # our total number of pixels
-    pixels = THUMB_WIDTH*THUMB_LENGTH*len(thumbs)
+    pixels = THUMB_WIDTH * THUMB_LENGTH * len(thumbs)
 
-    h = (pixels * horizontal) ** (1/2.0)
-    v = (pixels * vertical) ** (1/2.0)
+    h = (pixels * horizontal) ** (1 / 2.0)
+    v = (pixels * vertical) ** (1 / 2.0)
 
     # trim excess pixels from borders
-    return (int(h) - (int(h)%THUMB_WIDTH), int(v) - (int(v)%THUMB_WIDTH))
+    return (int(h) - (int(h) % THUMB_WIDTH), int(v) - (int(v) % THUMB_WIDTH))
+
 
 def normalize_thumbs(thumbs, max_thumbs=119):
-    ''' Set a maximum thumbnail allowance. Goal is to reduce calls
-        to the discogs api. This also translates to a maximum of 
-        1500x1000 image size'''
+    """Set a maximum thumbnail allowance. Goal is to reduce calls
+    to the discogs api. This also translates to a maximum of
+    1500x1000 image size"""
 
     # truncate and shuffle our thumbs if the max allowable size is greater
     # than we expect
     if len(thumbs) > max_thumbs:
-        logger.info('Truncating thumbs to {max_thumbs}.'.format(
-                max_thumbs=max_thumbs))
+        logger.info("Truncating thumbs to {max_thumbs}.".format(max_thumbs=max_thumbs))
         random.shuffle(thumbs)
         thumbs = thumbs[:max_thumbs]
 
     return thumbs
 
+
 def create_image(config, image_name, thumbs, horizontal=1024, vertical=576):
 
     # create a blank image canvas
-    new_image = Image.new('RGB', (horizontal, vertical))
-   
+    new_image = Image.new("RGB", (horizontal, vertical))
+
     # build the image. starting at position 0,0 stepping the thumbnail
     # width on each iteration of the loop
-    for i in xrange(0, horizontal, THUMB_WIDTH):
-        for j in xrange(0, vertical, THUMB_WIDTH):
+    for i in range(0, horizontal, THUMB_WIDTH):
+        for j in range(0, vertical, THUMB_WIDTH):
 
             try:
-                image_file = os.path.basename(
-                        thumbs.pop(random.randrange(len(thumbs))))
+                image_file = os.path.basename(thumbs.pop(random.randrange(len(thumbs))))
             except ValueError:
-                logger.error('empty list, breaking out of loop.')
+                logger.error("empty list, breaking out of loop.")
                 break
 
             # obtain a random thumbnail image from our list.
@@ -80,12 +74,13 @@ def create_image(config, image_name, thumbs, horizontal=1024, vertical=576):
             try:
                 thumb = Image.open(
                     os.path.join(
-                        config.get('discogs-banner', 'cache_directory'),
-                        image_file)
+                        config.get("discogs-banner", "cache_directory"), image_file
+                    )
                 )
             except IOError:
-                logger.error('Unable to open file {image}, skipping.'.format(
-                    image=image_file))
+                logger.error(
+                    "Unable to open file {image}, skipping.".format(image=image_file)
+                )
                 continue
             thumb.thumbnail((THUMB_WIDTH, THUMB_WIDTH), Image.ANTIALIAS)
 
--- tests/test_image_caculations.py
+++ tests/test_image_caculations.py
@@ -1,34 +1,39 @@
-import unittest
-import random
 import logging
+import random
+import unittest
 
-from discogs_banner.canvas_tools import normalize_thumbs, calculate_canvas
+from discogs_banner.canvas_tools import calculate_canvas, normalize_thumbs
 
 logger = logging.getLogger(__name__)
 logging.disable(logging.CRITICAL)
 
+
 class TestThumbRestrictions(unittest.TestCase):
 
     def setUp(self):
-      self.thumbs = normalize_thumbs(
-              [random.randint(0,150) for r in xrange(150)])
+        self.thumbs = normalize_thumbs([random.randint(0, 150) for r in range(150)])
 
     def test_thumb_length(self):
-      self.assertTrue(len(self.thumbs) == 119)
+        self.assertTrue(len(self.thumbs) == 119)
+
 
 class TestCanvasSizes(unittest.TestCase):
 
     def setUp(self):
-        ''' 16x9, 4x3, 2x1 '''
+        """16x9, 4x3, 2x1"""
 
         self.twobyone = calculate_canvas(
-                [random.randint(0,50) for r in xrange(50)], aspect='2x1')
+            [random.randint(0, 50) for r in range(50)], aspect="2x1"
+        )
         self.fourbythree = calculate_canvas(
-                [random.randint(0,50) for r in xrange(50)], aspect='4x3')
+            [random.randint(0, 50) for r in range(50)], aspect="4x3"
+        )
         self.sixteenbynine = calculate_canvas(
-                [random.randint(0,50) for r in xrange(50)], aspect='16x9')
+            [random.randint(0, 50) for r in range(50)], aspect="16x9"
+        )
         self.invalidratio = calculate_canvas(
-                [random.randint(0,50) for r in xrange(50)], aspect='1x1')
+            [random.randint(0, 50) for r in range(50)], aspect="1x1"
+        )
 
     def test_horizontal_size_2x1(self):
         self.assertTrue(self.twobyone[0] == 1000)
@@ -54,5 +59,6 @@ class TestCanvasSizes(unittest.TestCase):
     def test_invalid_verical(self):
         self.assertTrue(self.invalidratio[1] == None)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
